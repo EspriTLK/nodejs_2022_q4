b2e60a4a11fd60ae55e529a174266db88444e431
EspriTLK <esprit@inbox.ru>
2022-11-30 20:41:32 +0300

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile, stat } from 'fs/promises'
+
 const create = async () => {
     // Write your code here 
+    const _filePath = new URL('files', import.meta.url)
+    const _fileName = 'fresh.txt'
+
+    try {
+        const fileExist = async path => !!(await stat(path).catch(e => false));
+
+        if (await fileExist(`${_filePath.pathname}/${_fileName}`)) {
+            throw new Error('FS operation failed')
+        }
+
+        await writeFile(`${_filePath.pathname}/${_fileName}`, 'I am fresh and young', 'utf8')
+
+    } catch (error) {
+        console.log(error.message)
+    }
 };
 
 await create();
