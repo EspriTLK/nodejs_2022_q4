e30329f00989be9f8520d345067020886ba1ff3f
EspriTLK <esprit@inbox.ru>
2022-12-03 21:11:40 +0300

    feat: add cli, hash, modules

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'process'
+
 const parseArgs = () => {
     // Write your code here 
+    const args = []
+    argv.forEach((elm, i) => {
+        if(elm.startsWith('-')) {
+            args.push(`${elm.slice(2)} is ${argv[i+1]}`)
+        }
+    })
+    console.log(args.join(', '))
 };
 
 parseArgs();
